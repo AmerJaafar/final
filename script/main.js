bf9e2ab9e3d09e603a916143540f6df303fac45c
Hekmat Alawad <hekmat.alawad@dirs21.team>
2023-05-24 21:07:45 +0200

    login

--- script/main.js
+++ script/main.js
@@ -8,3 +8,20 @@ function togglePassword() {
       passwordInput.type = "password";
     }
   }
+
+
+let isAdmin = document.querySelector('#admin').getAttribute("id");
+let isTeacher = document.querySelector('#teacher').getAttribute("id")
+let isStudent = document.querySelector('#student').getAttribute("id")
+
+function getLoginPopup() {
+  if (isAdmin == "admin") {
+    console.log("isAdmin:", true);
+  } else if (isTeacher == "teacher") {
+    console.log("isTeacher:", true);
+  } else if (isStudent == "student") {
+    console.log("isStudent:", true);
+  } else {
+    console.log("not found");
+  }
+}
