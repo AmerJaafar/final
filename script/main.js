aa7104234dc075c29723b26a594e91ed511f60e5
Hekmat Alawad <hekmatalawad@gmx.de>
2023-05-24 23:46:36 +0200

    login page & popup func

--- script/main.js
+++ script/main.js
@@ -1,27 +1,53 @@
-function togglePassword() {
-    var passwordInput = document.getElementById("password");
-    var toggleButton = document.getElementById("toggle");
+function getLoginPopup(item) {
+  var popupDisplay = document.querySelector(".popup-container");
+  var welcomeMsg = document.querySelector("#welcome-msg");
 
-    if (toggleButton.checked) {
-      passwordInput.type = "text";
-    } else {
-      passwordInput.type = "password";
-    }
+  switch (item) {
+    case 'admin':
+      popupDisplay.style.display = "block";
+      welcomeMsg.innerHTML = "Welcome Admin";
+      break;
+    case 'teacher':
+      popupDisplay.style.display = "block";
+      welcomeMsg.innerHTML = "Welcome Teacher";
+      break;
+    case 'student':
+      popupDisplay.style.display = "block";
+      welcomeMsg.innerHTML = "Welcome Student";
+      break;
+    default:
+      console.log("not Found");
+      break;
   }
 
+}
 
-let isAdmin = document.querySelector('#admin').getAttribute("id");
-let isTeacher = document.querySelector('#teacher').getAttribute("id")
-let isStudent = document.querySelector('#student').getAttribute("id")
+function popupClose() {
+  var popupDisplay = document.querySelector(".popup-container");
+  popupDisplay.style.display = "none";
+}
 
-function getLoginPopup() {
-  if (isAdmin == "admin") {
-    console.log("isAdmin:", true);
-  } else if (isTeacher == "teacher") {
-    console.log("isTeacher:", true);
-  } else if (isStudent == "student") {
-    console.log("isStudent:", true);
+function togglePassword() {
+  var passType = document.querySelector("#pass");
+  var showPass = document.querySelector("i.fa-solid.fa-eye-slash.show-pass");
+  var hidePass = document.querySelector("i.fa-solid.fa-eye.action-icon.show-pass");
+
+  var labelPass = document.querySelector(".label-pass").style;
+  var inputPass = document.querySelector("input#pass").style;
+
+  if (passType.type == "password") {
+    passType.type = "text";
+    showPass.style.display = "inline-block";
+    hidePass.style.display = "none";
+
+    labelPass.background = "transparent"
+    inputPass.background = "transparent"
   } else {
-    console.log("not found");
+    passType.type = "password";
+    showPass.style.display = "none";
+    hidePass.style.display = "inline-block";
+
+    labelPass.background = "#dddddd"
+    inputPass.background = "#dddddd"
   }
 }
